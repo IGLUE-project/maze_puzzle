2c8de650ee468f696df643e415aadf208550bf77
Javier Ferreño <javifc67@gmail.com>
2025-04-24 12:07:37 +0200

    añadido sonido reset y mejora visual de fail

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -4,13 +4,18 @@ import "./../assets/scss/MainScreen.scss";
 export default function MainScreen({ maze, lastButtonClicked, clickButton, resetButton, mazeMap, show, config }) {
   const ClickButton = (button) => {
     clickButton(button);
-    const clickAudio = document.getElementById("audio_click");
-    clickAudio.play();
+    document.getElementById("audio_click").play();
+  };
+
+  const reset = () => {
+    resetButton();
+    document.getElementById("audio_reset").play();
   };
 
   return (
     <div id="MainScreen" className={"screen_wrapper" + (show ? "" : " screen_hidden")}>
       <audio id="audio_click" src="sounds/click_button.wav" autostart="false" preload="auto" />
+      <audio id="audio_reset" src="sounds/reset.wav" autostart="false" preload="auto" />
       <div className={`${config.theme?.keypadImg ? "image" : "frame"}`}>
         <div className="border-frame">
           <Maze
@@ -21,8 +26,8 @@ export default function MainScreen({ maze, lastButtonClicked, clickButton, reset
             mazeMap={mazeMap}
           />
           <div className="background"></div>
-          {/* <img src="/src/assets/images/reset.png" className="reset" onClick={resetButton} /> */}
         </div>
+        <img src="/src/assets/images/reset.png" className="reset" onClick={reset} />
         <img className="keypad" src={config.theme?.openKeypadImg} alt="" />
       </div>
     </div>
